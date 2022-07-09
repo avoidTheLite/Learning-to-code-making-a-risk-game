99a2755769c556754ef21f90631dd36f9c18c7fb
Justin Flanders <justin.flanders@aetna.com>
2022-07-09 10:41:06 -0600

    Add files via upload
    spent a night hacking this together without knowing even the most basic crap, but it was fun!

---
+++ risk.js
@@ -0,0 +1,67 @@
+function rollCombat(a,d)
+{
+
+const Attacker_output = []
+const Defender_output = []
+const attacker_count = a;
+const defender_count = d;
+
+
+//Roll Attacking Dice
+var i = 0;
+while (i<attacker_count){
+Attacker_output.push(rollDie());
+i++;
+}
+Attacker_output.sort().reverse();
+
+//Roll Defending Dice
+i = 0;
+while (i<defender_count){
+Defender_output.push(rollDie());
+i++;
+}
+Defender_output.sort().reverse();
+console.log (`Attacker rolled: ${Attacker_output}`);
+console.log (`Defender rolled: ${Defender_output}`);
+
+//Combat
+var minDice = Math.min(attacker_count,defender_count);
+var Attacker_lost_armies = 0
+var Defender_lost_armies = 0
+i=0 
+while (i<minDice){
+if(Attacker_output[i]>Defender_output[i]){
+Defender_lost_armies++
+}
+else
+{
+Attacker_lost_armies++
+}
+i++
+}
+console.log("Attacker loses "+Attacker_lost_armies+" army/armies");
+console.log("Defender loses "+Defender_lost_armies+" army/armies");
+
+//Decide winner
+var winner = "nobody";
+if(Attacker_lost_armies>Defender_lost_armies){
+winner = "Defender"
+}
+else{
+    if(Defender_lost_armies>Attacker_lost_armies){
+        winner = "Attacker"
+    }
+}
+
+console.log("Congratulations "+ winner);
+}
+
+
+function rollDie() {
+    var dieOutput = Math.ceil(6*Math.random());
+    return dieOutput;
+    
+}
+
+rollCombat(3,2);
